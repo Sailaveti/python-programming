f3cc7a104923adc5a2bcec60bc7e90fa52509c33
Sai laveti <sailaveti13@gmail.com>
2026-03-14 19:21:40 +0530

    all are python programs

---
+++ program.py
@@ -0,0 +1,5 @@
+num = float(input("Enter the Number: "))
+if (num%2==0):
+    print("Entered number is Even..")
+else:
+    print("Entered number is Odd..")
---
+++ program_1.py
@@ -0,0 +1,3 @@
+a=20
+if (a>18):
+    print("You are adult..")
